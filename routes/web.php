11726b3761f5caa8d05777eafa88dc8886290f0e
Kgs Wahyu <kgswahyu80@gmail.com>
2023-01-18 09:54:12 +0700

    Test File

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test-kgs', function () {
+    return "Halo ini Kgs. Wahyu";
+});
