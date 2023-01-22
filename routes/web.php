035263ab346af50e20b61f511e2528e54d783e2d
Kgs Wahyu <kgswahyu80@gmail.com>
2023-01-22 13:05:21 +0700

    Menambah semua route

--- routes/web.php
+++ routes/web.php
@@ -61,6 +61,4 @@ route::middleware(['auth','role:admin'])->group(function(){
 
 require __DIR__.'/auth.php';
 
-Auth::routes();
-
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
